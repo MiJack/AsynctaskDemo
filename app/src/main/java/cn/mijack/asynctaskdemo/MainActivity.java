76baab938dc009f4067e06c3ca5064bf5815d8aa
mijack <mijacstudio@gmail.com>
2018-07-25 23:14:54 +0800

    add log code

--- app/src/main/java/cn/mijack/asynctaskdemo/MainActivity.java
+++ app/src/main/java/cn/mijack/asynctaskdemo/MainActivity.java
@@ -8,10 +8,10 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        com.mijack.Xlog.logMethodEnter("void cn.mijack.asynctaskdemo.MainActivity.onCreate(android.os.Bundle)",this,savedInstanceState);try{super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Task12().execute(10);
         new Task1().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-        new Task1().execute();
+        new Task1().execute();com.mijack.Xlog.logMethodExit("void cn.mijack.asynctaskdemo.MainActivity.onCreate(android.os.Bundle)",this);}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("void cn.mijack.asynctaskdemo.MainActivity.onCreate(android.os.Bundle)",this,throwable);throw throwable;}
     }
 }
--- app/src/main/java/cn/mijack/asynctaskdemo/Task1.java
+++ app/src/main/java/cn/mijack/asynctaskdemo/Task1.java
@@ -9,15 +9,15 @@ import android.os.AsyncTask;
 public class Task1 extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... strings) {
-doSomeThing();
-        return "Hello world!";
+com.mijack.Xlog.logMethodEnter("java.lang.String cn.mijack.asynctaskdemo.Task1.doInBackground([java.lang.String)",this,strings);try{doSomeThing();
+        {com.mijack.Xlog.logMethodExit("java.lang.String cn.mijack.asynctaskdemo.Task1.doInBackground([java.lang.String)",this);return "Hello world!";}}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("java.lang.String cn.mijack.asynctaskdemo.Task1.doInBackground([java.lang.String)",this,throwable);throw throwable;}
     }
 
     public void doSomeThing() {
-        try {
+        com.mijack.Xlog.logMethodEnter("void cn.mijack.asynctaskdemo.Task1.doSomeThing()",this);try{try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }com.mijack.Xlog.logMethodExit("void cn.mijack.asynctaskdemo.Task1.doSomeThing()",this);}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("void cn.mijack.asynctaskdemo.Task1.doSomeThing()",this,throwable);throw throwable;}
     }
 }
