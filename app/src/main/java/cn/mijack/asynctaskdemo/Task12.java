6add7bf463931a16fdc7927db0f2f4b39baac2c1
mijack <mijacstudio@gmail.com>
2018-07-26 01:32:29 +0800

    add log code

--- app/src/main/java/cn/mijack/asynctaskdemo/Task12.java
+++ app/src/main/java/cn/mijack/asynctaskdemo/Task12.java
@@ -9,27 +9,27 @@ import android.os.AsyncTask;
 public class Task12 extends AsyncTask<Integer, Integer, String> {
 
     public void doSomeThing() {
-        try {
+        com.mijack.Xlog.logMethodEnter("void cn.mijack.asynctaskdemo.Task12.doSomeThing()",this);try{try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }com.mijack.Xlog.logMethodExit("void cn.mijack.asynctaskdemo.Task12.doSomeThing()",this);}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("void cn.mijack.asynctaskdemo.Task12.doSomeThing()",this,throwable);throw throwable;}
     }
 
 
     @Override
     protected String doInBackground(Integer... integers) {
-        StringBuffer sb = new StringBuffer();
+        com.mijack.Xlog.logMethodEnter("java.lang.String cn.mijack.asynctaskdemo.Task12.doInBackground([java.lang.Integer)",this,integers);try{StringBuffer sb = new StringBuffer();
         for (int i = 0; i < integers[0]; i++) {
             doSomeThing();
             sb.append(i);
             publishProgress(i);
         }
-        return sb.toString();
+        {com.mijack.Xlog.logMethodExit("java.lang.String cn.mijack.asynctaskdemo.Task12.doInBackground([java.lang.Integer)",this);return sb.toString();}}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("java.lang.String cn.mijack.asynctaskdemo.Task12.doInBackground([java.lang.Integer)",this,throwable);throw throwable;}
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
-        System.out.println("onProgressUpdate:" + values[0]);
+        com.mijack.Xlog.logMethodEnter("void cn.mijack.asynctaskdemo.Task12.onProgressUpdate([java.lang.Integer)",this,values);try{System.out.println("onProgressUpdate:" + values[0]);com.mijack.Xlog.logMethodExit("void cn.mijack.asynctaskdemo.Task12.onProgressUpdate([java.lang.Integer)",this);}catch(Throwable throwable){com.mijack.Xlog.logMethodExitWithThrowable("void cn.mijack.asynctaskdemo.Task12.onProgressUpdate([java.lang.Integer)",this,throwable);throw throwable;}
     }
 }
